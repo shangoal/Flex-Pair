c7cbd1208b36d41f9e756e96be11e2b4056e1f0b
Flex-MD <sandarunethshanhome@gmail.com>
2025-07-15 17:14:57 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://github.com/shangoal/Flex-MD/blob/main/images/flex%20Music.jpg?raw=true",
               },
               caption: sid,
             });
