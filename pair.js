8640c86a9583393d2c7ad97eb3b877cbb96fa882
Flex-MD <sandarunethshanhome@gmail.com>
2025-07-21 13:57:01 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -20,10 +20,10 @@ function removeFile(FilePath) {
 
 router.get("/", async (req, res) => {
   let num = req.query.number;
-  async function RobinPair() {
+  async function FlexPair() {
     const { state, saveCreds } = await useMultiFileAuthState(`./session`);
     try {
-      let RobinPairWeb = makeWASocket({
+      let FlexPairWeb = makeWASocket({
         auth: {
           creds: state.creds,
           keys: makeCacheableSignalKeyStore(
@@ -36,17 +36,17 @@ router.get("/", async (req, res) => {
         browser: Browsers.macOS("Safari"),
       });
 
-      if (!RobinPairWeb.authState.creds.registered) {
+      if (!FlexPairWeb.authState.creds.registered) {
         await delay(1500);
         num = num.replace(/[^0-9]/g, "");
-        const code = await RobinPairWeb.requestPairingCode(num);
+        const code = await FlexPairWeb.requestPairingCode(num);
         if (!res.headersSent) {
           await res.send({ code });
         }
       }
 
-      RobinPairWeb.ev.on("creds.update", saveCreds);
-      RobinPairWeb.ev.on("connection.update", async (s) => {
+      FlexPairWeb.ev.on("creds.update", saveCreds);
+      FlexPairWeb.ev.on("connection.update", async (s) => {
         const { connection, lastDisconnect } = s;
         if (connection === "open") {
           try {
@@ -54,7 +54,7 @@ router.get("/", async (req, res) => {
             const sessionPrabath = fs.readFileSync("./session/creds.json");
 
             const auth_path = "./session/";
-            const user_jid = jidNormalizedUser(RobinPairWeb.user.id);
+            const user_jid = jidNormalizedUser(FlexPairWeb.user.id);
 
             function randomMegaId(length = 6, numberLength = 4) {
               const characters =
@@ -81,18 +81,18 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*FLEX [The Music Crew]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.link/wesn0e*\n\n*You can join my whatsapp channel*\n\n*https://whatsapp.com/channel/0029VafGSncKLaHqTUlwhu2t*`;
+            const sid = `*Flex Web Pair*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link👇*\n\n*wa.link/wesn0e*\n\n*You can join my whatsapp channel👇*\n\n*https://whatsapp.com/channel/0029VafGSncKLaHqTUlwhu2t*`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
-            const dt = await RobinPairWeb.sendMessage(user_jid, {
+            const dt = await FlexPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://github.com/shangoal/Flex-MD/blob/main/images/flex%20Music.jpg?raw=true",
+                url: "https://github.com/shangoal/Flex-Pair/blob/main/images/Flex%20Team%20Logo.jpg?raw=true",
               },
               caption: sid,
             });
-            const msg = await RobinPairWeb.sendMessage(user_jid, {
+            const msg = await FlexPairWeb.sendMessage(user_jid, {
               text: string_session,
             });
-            const msg1 = await RobinPairWeb.sendMessage(user_jid, { text: mg });
+            const msg1 = await FlexPairWeb.sendMessage(user_jid, { text: mg });
           } catch (e) {
             exec("pm2 restart prabath");
           }
@@ -107,20 +107,20 @@ router.get("/", async (req, res) => {
           lastDisconnect.error.output.statusCode !== 401
         ) {
           await delay(10000);
-          RobinPair();
+          FlexPair();
         }
       });
     } catch (err) {
       exec("pm2 restart Robin-md");
       console.log("service restarted");
-      RobinPair();
+      FlexPair();
       await removeFile("./session");
       if (!res.headersSent) {
         await res.send({ code: "Service Unavailable" });
       }
     }
   }
-  return await RobinPair();
+  return await FlexPair();
 });
 
 process.on("uncaughtException", function (err) {
