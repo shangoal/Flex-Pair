28d9b26aa447c1a271d8f4863400f537045e31d6
Flex-MD <sandarunethshanhome@gmail.com>
2025-07-21 14:03:31 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,131 +1,131 @@
-const express = require("express");
-const fs = require("fs");
-const { exec } = require("child_process");
+const express = require(express);
+const fs = require(fs);
+const { exec } = require(child_process);
 let router = express.Router();
-const pino = require("pino");
+const pino = require(pino);
 const {
-  default: makeWASocket,
+  default makeWASocket,
   useMultiFileAuthState,
   delay,
   makeCacheableSignalKeyStore,
   Browsers,
   jidNormalizedUser,
-} = require("@whiskeysockets/baileys");
-const { upload } = require("./mega");
+} = require(@whiskeysocketsbaileys);
+const { upload } = require(.mega);
 
 function removeFile(FilePath) {
   if (!fs.existsSync(FilePath)) return false;
-  fs.rmSync(FilePath, { recursive: true, force: true });
+  fs.rmSync(FilePath, { recursive true, force true });
 }
 
-router.get("/", async (req, res) => {
+router.get(, async (req, res) = {
   let num = req.query.number;
-  async function FlexPair() {
-    const { state, saveCreds } = await useMultiFileAuthState(`./session`);
+  async function RobinPair() {
+    const { state, saveCreds } = await useMultiFileAuthState(`.session`);
     try {
-      let FlexPairWeb = makeWASocket({
-        auth: {
-          creds: state.creds,
-          keys: makeCacheableSignalKeyStore(
+      let RobinPairWeb = makeWASocket({
+        auth {
+          creds state.creds,
+          keys makeCacheableSignalKeyStore(
             state.keys,
-            pino({ level: "fatal" }).child({ level: "fatal" })
+            pino({ level fatal }).child({ level fatal })
           ),
         },
-        printQRInTerminal: false,
-        logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-        browser: Browsers.macOS("Safari"),
+        printQRInTerminal false,
+        logger pino({ level fatal }).child({ level fatal }),
+        browser Browsers.macOS(Safari),
       });
 
-      if (!FlexPairWeb.authState.creds.registered) {
+      if (!RobinPairWeb.authState.creds.registered) {
         await delay(1500);
-        num = num.replace(/[^0-9]/g, "");
-        const code = await FlexPairWeb.requestPairingCode(num);
+        num = num.replace([^0-9]g, );
+        const code = await RobinPairWeb.requestPairingCode(num);
         if (!res.headersSent) {
           await res.send({ code });
         }
       }
 
-      FlexPairWeb.ev.on("creds.update", saveCreds);
-      FlexPairWeb.ev.on("connection.update", async (s) => {
+      RobinPairWeb.ev.on(creds.update, saveCreds);
+      RobinPairWeb.ev.on(connection.update, async (s) = {
         const { connection, lastDisconnect } = s;
-        if (connection === "open") {
+        if (connection === open) {
           try {
             await delay(10000);
-            const sessionPrabath = fs.readFileSync("./session/creds.json");
+            const sessionPrabath = fs.readFileSync(.sessioncreds.json);
 
-            const auth_path = "./session/";
-            const user_jid = jidNormalizedUser(FlexPairWeb.user.id);
+            const auth_path = .session;
+            const user_jid = jidNormalizedUser(RobinPairWeb.user.id);
 
             function randomMegaId(length = 6, numberLength = 4) {
               const characters =
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-              let result = "";
-              for (let i = 0; i < length; i++) {
+                ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789;
+              let result = ;
+              for (let i = 0; i  length; i++) {
                 result += characters.charAt(
-                  Math.floor(Math.random() * characters.length)
+                  Math.floor(Math.random()  characters.length)
                 );
               }
               const number = Math.floor(
-                Math.random() * Math.pow(10, numberLength)
+                Math.random()  Math.pow(10, numberLength)
               );
               return `${result}${number}`;
             }
 
             const mega_url = await upload(
-              fs.createReadStream(auth_path + "creds.json"),
+              fs.createReadStream(auth_path + creds.json),
               `${randomMegaId()}.json`
             );
 
             const string_session = mega_url.replace(
-              "https://mega.nz/file/",
-              ""
+              httpsmega.nzfile,
+              
             );
 
-            const sid = `*Flex Web Pair*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link👇*\n\n*wa.link/wesn0e*\n\n*You can join my whatsapp channel👇*\n\n*https://whatsapp.com/channel/0029VafGSncKLaHqTUlwhu2t*`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑`;
-            const dt = await FlexPairWeb.sendMessage(user_jid, {
-              image: {
-                url: "https://github.com/shangoal/Flex-Pair/blob/main/images/Flex%20Team%20Logo.jpg?raw=true",
+            const sid = `FLEX [The Music Crew]nn👉 ${string_session} 👈nnThis is the your Session ID, copy this id and paste into config.js filennYou can ask any question using this linknnwa.linkwesn0ennYou can join my whatsapp channelnnhttpswhatsapp.comchannel0029VafGSncKLaHqTUlwhu2t`;
+            const mg = `🛑 Do not share this code to anyone 🛑`;
+            const dt = await RobinPairWeb.sendMessage(user_jid, {
+              image {
+                url httpsgithub.comshangoalFlex-MDblobmainimagesflex%20Music.jpgraw=true,
               },
-              caption: sid,
+              caption sid,
             });
-            const msg = await FlexPairWeb.sendMessage(user_jid, {
-              text: string_session,
+            const msg = await RobinPairWeb.sendMessage(user_jid, {
+              text string_session,
             });
-            const msg1 = await FlexPairWeb.sendMessage(user_jid, { text: mg });
+            const msg1 = await RobinPairWeb.sendMessage(user_jid, { text mg });
           } catch (e) {
-            exec("pm2 restart prabath");
+            exec(pm2 restart prabath);
           }
 
           await delay(100);
-          return await removeFile("./session");
+          return await removeFile(.session);
           process.exit(0);
         } else if (
-          connection === "close" &&
+          connection === close &&
           lastDisconnect &&
           lastDisconnect.error &&
           lastDisconnect.error.output.statusCode !== 401
         ) {
           await delay(10000);
-          FlexPair();
+          RobinPair();
         }
       });
     } catch (err) {
-      exec("pm2 restart Robin-md");
-      console.log("service restarted");
-      FlexPair();
-      await removeFile("./session");
+      exec(pm2 restart Robin-md);
+      console.log(service restarted);
+      RobinPair();
+      await removeFile(.session);
       if (!res.headersSent) {
-        await res.send({ code: "Service Unavailable" });
+        await res.send({ code Service Unavailable });
       }
     }
   }
-  return await FlexPair();
+  return await RobinPair();
 });
 
-process.on("uncaughtException", function (err) {
-  console.log("Caught exception: " + err);
-  exec("pm2 restart Robin");
+process.on(uncaughtException, function (err) {
+  console.log(Caught exception  + err);
+  exec(pm2 restart Robin);
 });
 
 module.exports = router;
